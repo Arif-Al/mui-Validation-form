537e2a578a887fc5400f931c1d571f166005db4f
Arir Al <you@example.comgit config --global user.name Your>
2024-11-04 13:25:51 +0500

    complete first MUI assignment

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import FormComponent from './components/FormComponent';
+
+
+
+function App() {
+  return (
+    <div className="App">
+ <FormComponent /> 
+
+    </div>
+  );
+}
+
+export default App;
